1b0e2a42839fe7f6554fad7fa0339ab356174af6
Michael Vernau <102547230+MichaelRuns@users.noreply.github.com>
2023-12-01 18:03:21 -0800

    updates

--- src/App.js
+++ src/App.js
@@ -7,28 +7,28 @@ function App() {
   return (
    <div className='bg-slate-900 grid grid-rows-[10%,90%]'>
     <div className='bg-blue-400 flex flex-row w-1/4 bg-white border border-4 border-yellow-300'>
-      <img  className={'h-full border-0 border-transparent'} src={process.env.PUBLIC_URL + '/kitten_logo.jpeg'} alt="Example" />
-      <div className='flex items-center justify-center font-semibold text-2xl italic font-sans  '> {`UCD Kitten Lab`}</div>
+      <img  className={'h-3/4 md:h-full -translate-x-5 md:translate-x-0 border-0 border-transparent'} src={process.env.PUBLIC_URL + '/kitten_logo.jpeg'} alt="Example" />
+      <div className='flex items-center justify-center -translate-x-5 md:translate-x-0 font-semibold text-sm italic font-sans md:text-2xl'> {`Vernau Kitten Lab`}</div>
     </div>
     <div>
       <Tab.Group>
       <Tab.List className={'w-screen flex justify-between gap-2 p-1 bg-slate-100'}>
         <Tab className={({ selected }) =>
-      `py-2 px-4 w-1/4 cursor-pointer ${
+      `py-2 px-4 w-1/4 cursor-pointer  flex items-center justify-center ${
         selected
           ? 'bg-gray-800 text-white'
           : 'bg-gray-200 text-gray-800'
       }`
     }> About Us</Tab>
         <Tab className={({ selected }) =>
-      `py-2 px-4 w-1/4 cursor-pointer ${
+      `py-2 px-4 w-1/4 cursor-pointer flex items-center justify-center ${
         selected
           ? 'bg-gray-800 text-white'
           : 'bg-gray-200 text-gray-800'
       }`
     }> Programs</Tab>
         <Tab className={({ selected }) =>
-      `py-2 px-4 w-1/4 cursor-pointer ${
+      `py-2 px-4 w-1/4 cursor-pointer flex items-center justify-center ${
         selected
           ? 'bg-gray-800 text-white'
           : 'bg-gray-200 text-gray-800'
--- src/panels/AboutPanel.js
+++ src/panels/AboutPanel.js
@@ -1,18 +1,55 @@
 import { CameraIcon } from '@heroicons/react/outline'
+
+const doctors = [
+  {
+    first_name: "Karen",
+    last_name: "Vernau",
+    picture: null,
+    bio: "lorem ipsum haha xd"
+    },
+    {
+      first_name: "Doctor",
+      last_name: "McDoctorson",
+      picture: null,
+      bio:"does doctor things"
+    },
+    {
+      first_name: "Doctor",
+      last_name: "McDoctorson",
+      picture: null,
+      bio:"does doctor things"
+    },
+    {
+      first_name: "Doctor",
+      last_name: "McDoctorson",
+      picture: null,
+      bio:"does doctor things"
+    },
+]
 function AboutPanel() {
   return (
-    <div className="w-full h-screen flex flex-col items-center justify-center rounded p-2">
-            <div className="w-4/5 h-4/5 bg-slate-200 rounded grid grid-rows-1 grid-cols-2">
+    <div className="w-full h-full flex flex-col gap-2 items-center justify-center rounded p-2">
+            <div className="w-4/5 bg-slate-200 rounded grid grid-rows-2 grid-cols-1 md:grid-rows-1 md:grid-cols-2">
                 <CameraIcon className="w-full h-full bg-slate-900 text-white"/>
                 <div className='flex items-begin justify-begin'>
                     {"We are the kitten lab. We do kitten stuff."}
                 </div>
             </div>
-            <div> Dr.1</div>
-            <div> Dr.2</div>
-            <div> Dr.3</div>
-            <div> Dr.4</div>
-            <div> Dr.5</div>
+            {doctors.map((doctor) =>{
+              return (
+                <div className='bg-slate-200 grid grid-rows-3 grid-cols-1 w-2/3'>
+                  <div className='font-bold'>
+                    {doctor.first_name + " " + doctor.last_name}
+                  </div>
+                  <div>
+                    {doctor.picture? <img src={`{}`}/>: <CameraIcon className='w-5'/>}
+                  </div>
+                  <div>
+                    {doctor.bio}
+                  </div>
+                </div>
+              );
+            })}
     </div>
   );
 }
